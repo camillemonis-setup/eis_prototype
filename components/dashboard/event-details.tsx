64815cb109009635798b66fdd45e699e8e87d4ac
Camille <camille.monis@setup.com.ph>
2025-03-25 05:45:36 +0800

    test

--- components/dashboard/event-details.tsx
+++ components/dashboard/event-details.tsx
@@ -12,18 +12,18 @@ ModuleRegistry.registerModules([AllCommunityModule]);
 ModuleRegistry.registerModules([AllEnterpriseModule]);
 LicenseManager.setLicenseKey('[TRIAL]_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-078794}_is_granted_for_evaluation_only___Use_in_production_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_purchasing_a_production_key_please_contact_info@ag-grid.com___You_are_granted_a_{Single_Application}_Developer_License_for_one_application_only___All_Front-End_JavaScript_developers_working_on_the_application_would_need_to_be_licensed___This_key_will_deactivate_on_{14 April 2025}____[v3]_[0102]_MTc0NDU4NTIwMDAwMA==0e65fd8a353058a58afb8d7be064e726');
 
-import data from './exhibitor.json';
-import { Button, buttonVariants } from '../ui/button';
+import data from './event.json';
+import { Button } from '../ui/button';
 import { Edit2 } from 'lucide-react';
 import Link from 'next/link';
 
 const renderDropdown = (params: any) => {
     let bgColor;
     switch (params.value.toLowerCase()) {
-        case 'pending':
+        case 'stopped':
             bgColor = 'orange';
             break;
-        case 'incomplete':
+        case 'blocked':
             bgColor = 'red';
             break;
         default:
@@ -41,112 +41,18 @@ const EventTable = () => {
     const [rowData] = useState(data);
 
     const [columnDefs, setColumnDefs] = useState<ColDef[]>([
-        {
-            headerName: "Business",
-            pinned: 'left',
-            field: "businessLogo",
-            cellRenderer: (params: any) => {
-                return (
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                        <img src={params.value} alt='Logo' style={{ width: 40, height: 40, marginRight: 10 }} />
-                        <div>
-                            <strong>{params.data.university}</strong><br />
-                            <span>{params.data.location}</span>
-                        </div>
-                    </div>
-                )
-            }
-        },
-        {
-            headerName: "Contact person",
-            field: "contactPerson",
-            editable: true,
-            cellRenderer: (params: any) => {
-                return (
-                    <div>
-                        <a href='#' style={{ color: 'blue', textDecoration: 'none' }}>{params.value}</a><br />
-                        <span>{params.data.position}</span>
-                    </div>
-                )
-            }
-        },
-        {
-            headerName: "Booking status", editable: true,
-            cellEditor: "agSelectCellEditor",
-            cellEditorParams: {
-                values: ["PENDING", "INCOMPLETE", "DONE"],
-            },
-            field: "bookingStatus", cellRenderer: (params: any) => renderDropdown(params),
-            filter: 'agSetColumnFilter',
-            filterParams: {
-                values: ["PENDING", "INCOMPLETE", "COMPLETED"],
-            }
-        },
-
-        {
-            headerName: "Profile status",
-            editable: true,
-            cellEditor: "agSelectCellEditor",
-            cellEditorParams: {
-                values: ["PENDING", "INCOMPLETE", "COMPLETED"],
-            }, field: "profileStatus", cellRenderer: (params: any) => renderDropdown(params),
-            filter: 'agSetColumnFilter',
-            filterParams: {
-                values: ["PENDING", "INCOMPLETE", "COMPLETED"],
-            }
-        },
-        {
-            headerName: "Advert status", editable: true,
-            cellEditor: "agSelectCellEditor",
-            cellEditorParams: {
-                values: ["PENDING", "INCOMPLETE", "COMPLETED"],
-            }, field: "advertStatus", cellRenderer: (params: any) => renderDropdown(params),
-            filter: 'agSetColumnFilter',
-            filterParams: {
-                values: ["PENDING", "INCOMPLETE", "COMPLETED"],
-            }
-        },
-        {
-            headerName: "Badges status", editable: true,
-            cellEditor: "agSelectCellEditor",
-            cellEditorParams: {
-                values: ["PENDING", "INCOMPLETE", "COMPLETED"],
-            }, field: "badgesStatus", cellRenderer: (params: any) => renderDropdown(params),
-            filter: 'agSetColumnFilter',
-            filterParams: {
-                values: ["PENDING", "INCOMPLETE", "COMPLETED"],
-            }
-        },
-        { 
-            headerName: "Representative status", 
-            field: "representativeStatus", 
-            cellRenderer: (params: any) => renderDropdown(params),
-            filter: 'agSetColumnFilter',
-            filterParams: {
-                values: ["PENDING", "INCOMPLETE", "COMPLETED"],
-            }
-        },
-
-        {
-            headerName: "Booking date",
-            editable: true,
-            field: "bookingDate",
-            cellEditor: "agDateCellEditor",
-            cellEditorParams: {
-                // Optional: specify the date format
-                dateFormat: 'yyyy-mm-dd'
-            }
-        },
-        {
-            headerName: "Actions", pinned: 'right', field: "actions", width: 150, cellRenderer: () => {
-                return (
-                    <Button asChild>
-                        <Link href="/"><Edit2/></Link>
-                    </Button>
-
-                )
-            }
-        },
+        { headerName: "City Name", field: "city_name", sortable: true, filter: true, editable: true, pinned: 'left', width:130 },
+        { headerName: "Event Type", field: "event_type", sortable: true, filter: true, width: 150 },
+        { headerName: "Country Code", field: "country_code", sortable: true, filter: true, editable: true },
+        { headerName: "Start Date", field: "start_date", sortable: true, filter: "agDateColumnFilter", editable: true },
+        { headerName: "Start Time", field: "start_time", sortable: true, filter: true, editable: true },
+        { headerName: "Close Date", field: "close_date", sortable: true, filter: "agDateColumnFilter", editable: true },
+        { headerName: "Close Time", field: "close_time", sortable: true, filter: true, editable: true },
+        { headerName: "Status", field: "status", sortable: true, filter: true, editable: true, cellRenderer: (params: any) => renderDropdown(params), width: 130 },
+        { headerName: "Venue Name", field: "venue.name", sortable: true, filter: true, editable: true },
+        { headerName: "Venue Address", field: "venue.address", sortable: true, filter: true, editable: true },
+        { headerName: "Telephone", field: "venue.telephone", sortable: true, filter: true, editable: true },
+        { headerName: "Currency", field: "currency", sortable: true, filter: true, editable: true }
     ]);
 
     return (
